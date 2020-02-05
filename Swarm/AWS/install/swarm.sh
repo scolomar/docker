cc6732bf89c8a77e08e138f1ba0b163dce65e504
secobau <sebastian.colomar@gmail.com>
2020-02-05 09:23:42 +0100

    Update swarm.sh

--- Swarm/AWS/install/swarm.sh
+++ Swarm/AWS/install/swarm.sh
@@ -1,24 +1,10 @@
 #!/bin/bash
 
+pwd=$( dirname $( readlink -f $0 ) ) ;
+
 test -z "$stack" && echo PLEASE DEFINE THE VALUE FOR stack && exit 1 ;
 
-function send_list_command {
- local command="$1" ;
- local target="$2" ;
- local stack="$3" ;
- local CommandId=$( aws ssm send-command --document-name "AWS-RunShellScript" --parameters commands="$command" --targets Key=tag:"aws:cloudformation:stack-name",Values="$stack" Key=tag:"aws:cloudformation:logical-id",Values="$target" --query "Command.CommandId" --output text ) ;
- while true ; do
-  output=$( aws ssm list-command-invocations --command-id $CommandId --query "CommandInvocations[].CommandPlugins[].Output" --details --output text ) ;
-  echo $output | grep [a-zA-Z0-9] --quiet && break ;
- done ;
-}
- 
-function send_command {
- local command="$1" ;
- local target="$2" ;
- local stack="$3" ;
- local CommandId=$( aws ssm send-command --document-name "AWS-RunShellScript" --parameters commands="$command" --targets Key=tag:"aws:cloudformation:stack-name",Values="$stack" Key=tag:"aws:cloudformation:logical-id",Values="$target" --query "Command.CommandId" --output text ) ;
-}
+source source $pwd/docker/Swarm/AWS/common/functions.sh
 
 command=" sudo docker swarm join-token manager | grep token " ;
 targets=" InstanceManager1 " ;
