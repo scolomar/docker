272511be6181d141a7423116f1dc84ab1af9b53a
secobau <sebastian.colomar@gmail.com>
2020-02-05 08:21:47 +0100

    Update and rename Swarm/AWS/leader.sh to Swarm/AWS/install/leader.sh

--- Swarm/AWS/install/leader.sh
+++ Swarm/AWS/install/leader.sh
@@ -4,5 +4,3 @@ sudo amazon-linux-extras install docker -y
 sudo systemctl enable docker
 sudo systemctl start docker
 sudo docker swarm init
-sudo docker swarm join-token manager | grep token 1> token_manager
-sudo docker swarm join-token worker  | grep token 1> token_worker
