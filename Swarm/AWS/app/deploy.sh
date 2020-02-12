ec056927030e13442786cf989920f41e28564058
secobau <sebastian.colomar@gmail.com>
2020-02-12 23:18:52 +0100

    Update deploy.sh

--- Swarm/AWS/app/deploy.sh
+++ Swarm/AWS/app/deploy.sh
@@ -8,19 +8,19 @@ test -z "$stack" && echo PLEASE DEFINE THE VALUE FOR stack && exit 1 ;
 
 source $pwd/../common/functions.sh
 
-command=" cd \&\& git clone https://github.com/secobau/docker.git " ;
+command=' cd \&\& git clone https://github.com/secobau/docker.git ' ;
 targets=" InstanceManager1 " ;
 for target in $targets ; do
  send_command "$command" "$target" "$stack" ;
 done ;
 
-command=" cd \&\& sudo docker stack deploy -c docker/Swarm/AWS/app/php.yml php " ;
+command=' cd \&\& sudo docker stack deploy -c docker/Swarm/AWS/app/php.yml php ' ;
 targets=" InstanceManager1 " ;
 for target in $targets ; do
  send_command "$command" "$target" "$stack" ;
 done ;
 
-command=" cd \&\& sudo docker stack deploy -c docker/Swarm/AWS/app/dockercoins.yml dockercoins " ;
+command=' cd \&\& sudo docker stack deploy -c docker/Swarm/AWS/app/dockercoins.yml dockercoins ' ;
 targets=" InstanceManager1 " ;
 for target in $targets ; do
  send_command "$command" "$target" "$stack" ;
