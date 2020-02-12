360f54a629f054d10082cc34fd5b8e084c0b345c
secobau <sebastian.colomar@gmail.com>
2020-02-12 23:49:07 +0100

    Update deploy.sh

--- Swarm/AWS/app/deploy.sh
+++ Swarm/AWS/app/deploy.sh
@@ -8,19 +8,25 @@ test -z "$stack" && echo PLEASE DEFINE THE VALUE FOR stack && exit 1 ;
 
 source $pwd/../common/functions.sh
 
-command=" git clone https://github.com/secobau/docker.git docker-clone " ;
+command=" git clone https://github.com/secobau/docker.git docker-secobau " ;
 targets=" InstanceManager1 " ;
 for target in $targets ; do
  send_command "$command" "$target" "$stack" ;
 done ;
 
-command=" sudo docker stack deploy -c docker-clone/Swarm/AWS/app/php.yml php " ;
+command=" sudo docker stack deploy -c docker-secobau/Swarm/AWS/app/php.yml php " ;
 targets=" InstanceManager1 " ;
 for target in $targets ; do
  send_command "$command" "$target" "$stack" ;
 done ;
 
-command=" sudo docker stack deploy -c docker-clone/Swarm/AWS/app/dockercoins.yml dockercoins " ;
+command=" sudo docker stack deploy -c docker-secobau/Swarm/AWS/app/dockercoins.yml dockercoins " ;
+targets=" InstanceManager1 " ;
+for target in $targets ; do
+ send_command "$command" "$target" "$stack" ;
+done ;
+
+command=" sudo rm --recursive --force docker-secobau " ;
 targets=" InstanceManager1 " ;
 for target in $targets ; do
  send_command "$command" "$target" "$stack" ;
