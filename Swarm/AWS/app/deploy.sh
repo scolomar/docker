4b74df07b43241c11f45f383a7e64955a615d9b6
secobau <sebastian.colomar@gmail.com>
2020-02-12 23:07:38 +0100

    Update deploy.sh

--- Swarm/AWS/app/deploy.sh
+++ Swarm/AWS/app/deploy.sh
@@ -8,7 +8,19 @@ test -z "$stack" && echo PLEASE DEFINE THE VALUE FOR stack && exit 1 ;
 
 source $pwd/../common/functions.sh
 
-command="git clone https://github.com/secobau/ucp.git && sudo docker stack deploy -c ucp/aws/php.yml php && sudo docker stack deploy -c ucp/aws/dockercoins.yml dockercoins" ;
+command=" cd ; git clone https://github.com/secobau/docker.git " ;
+targets=" InstanceManager1 " ;
+for target in $targets ; do
+ send_command "$command" "$target" "$stack" ;
+done ;
+
+command=" cd ; sudo docker stack deploy -c docker/Swarm/AWS/app/php.yml php " ;
+targets=" InstanceManager1 " ;
+for target in $targets ; do
+ send_command "$command" "$target" "$stack" ;
+done ;
+
+command=" cd ; sudo docker stack deploy -c docker/Swarm/AWS/app/dockercoins.yml dockercoins " ;
 targets=" InstanceManager1 " ;
 for target in $targets ; do
  send_command "$command" "$target" "$stack" ;
