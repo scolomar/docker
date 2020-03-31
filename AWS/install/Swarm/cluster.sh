ae83409c3d5a1ce5f3725ea7e476f9252658c9d8
secobau <sebastian.colomar@gmail.com>
2020-03-31 16:40:10 +0200

    Update cluster.sh

--- AWS/install/Swarm/cluster.sh
+++ AWS/install/Swarm/cluster.sh
@@ -8,11 +8,31 @@ test -z "$stack" && echo PLEASE DEFINE THE VALUE FOR stack && exit 1 ;
 
 source $pwd/../../common/functions.sh
 
-command=" sudo docker swarm init | grep token " ;
+command=" \
+ sudo yum update -y \
+ && sudo amazon-linux-extras install docker -y \
+ && sudo systemctl enable docker \
+ && sudo systemctl start docker \
+ && sudo docker swarm init \
+ " ;
+targets=" \
+ InstanceManager1 \
+ InstanceManager2 \
+ InstanceManager3 \
+ InstanceWorker1 \
+ InstanceWorker2 \
+ InstanceWorker3 \
+ " ;
+for target in $targets ; do
+ send_command "$command" "$target" "$stack" ;
+done ;
+
+command=" sudo docker swarm init | grep token --max-count 1 " ;
 targets=" InstanceManager1 " ;
 for target in $targets ; do
  send_list_command "$command" "$target" "$stack" ;
 done ;
+token_worker=" $output ";
 
 command=" sudo docker swarm join-token manager | grep token " ;
 targets=" InstanceManager1 " ;
@@ -20,13 +40,6 @@ for target in $targets ; do
  send_list_command "$command" "$target" "$stack" ;
 done ;
 token_manager=" $output ";
-
-command=" sudo docker swarm join-token worker | grep token " ;
-targets=" InstanceManager1 " ;
-for target in $targets ; do
- send_list_command "$command" "$target" "$stack" ;
-done ;
-token_worker=" $output ";
 
 command=" sudo $token_manager " ;
 targets=" InstanceManager2 InstanceManager3 " ;
