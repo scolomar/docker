3bacaad4a7a98ab88cf9ccca4119350b1dfc45f7
secobau <sebastian.colomar@gmail.com>
2020-04-24 03:59:11 +0200

    Update functions.sh

--- AWS/common/functions.sh
+++ AWS/common/functions.sh
@@ -2,9 +2,24 @@ function send_list_command {
  local command="$1" ;
  local target="$2" ;
  local stack="$3" ;
- local CommandId=$( aws ssm send-command --document-name "AWS-RunShellScript" --parameters commands="$command" --targets Key=tag:"aws:cloudformation:stack-name",Values="$stack" Key=tag:"aws:cloudformation:logical-id",Values="$target" --query "Command.CommandId" --output text ) ;
+ local CommandId=$( \
+  aws ssm send-command \
+   --document-name "AWS-RunShellScript" \
+   --parameters commands="$command" \
+   --targets \
+    Key=tag:"aws:cloudformation:stack-name",Values="$stack" \
+    Key=tag:"aws:cloudformation:logical-id",Values="$target" \
+   --query "Command.CommandId" \
+   --output text \
+ ) ;
  while true ; do
-  output=$( aws ssm list-command-invocations --command-id $CommandId --query "CommandInvocations[].CommandPlugins[].Output" --details --output text ) ;
+  output=$( \
+   aws ssm list-command-invocations \
+    --command-id $CommandId \
+    --query "CommandInvocations[].CommandPlugins[].Output" \
+    --details \
+    --output text \
+  ) ;
   echo $output | grep [a-zA-Z0-9] --quiet && break ;
  done ;
 }
@@ -13,5 +28,14 @@ function send_command {
  local command="$1" ;
  local target="$2" ;
  local stack="$3" ;
- local CommandId=$( aws ssm send-command --document-name "AWS-RunShellScript" --parameters commands="$command" --targets Key=tag:"aws:cloudformation:stack-name",Values="$stack" Key=tag:"aws:cloudformation:logical-id",Values="$target" --query "Command.CommandId" --output text ) ;
+ local CommandId=$( \
+  aws ssm send-command \
+   --document-name "AWS-RunShellScript" \
+   --parameters commands="$command" \
+   --targets \
+    Key=tag:"aws:cloudformation:stack-name",Values="$stack" \
+    Key=tag:"aws:cloudformation:logical-id",Values="$target" \
+   --query "Command.CommandId" \
+   --output text \
+ ) ;
 }
