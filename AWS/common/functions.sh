5838127d3eacb39e38980c57b6fee801c7c34772
Sebastian Colomar <sebastian.colomar@gmail.com>
2020-05-10 09:11:25 +0200

    .

--- AWS/common/functions.sh
+++ AWS/common/functions.sh
@@ -19,8 +19,9 @@ function send_list_command {
     --query "CommandInvocations[].CommandPlugins[].Output" \
     --details \
     --output text \
+    2> /dev/null \
   ) ;
-  echo $output 2> /dev/null | grep [a-zA-Z0-9] --quiet && break ;
+  echo $output | grep [a-zA-Z0-9] --quiet && break ;
  done ;
  echo $output;
 }
