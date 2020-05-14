094ef8dce842522aa69ef342401569f24de83eb4
Sebastian Colomar <sebastian.colomar@gmail.com>
2020-05-14 07:50:25 +0200

    Merge init script

--- AWS/install/Swarm/cluster.sh
+++ AWS/install/Swarm/cluster.sh
@@ -13,7 +13,7 @@ test -z "$stack" && echo PLEASE DEFINE THE VALUE FOR stack && exit 1 	;
 pwd=$( dirname $( readlink -f $0 ) ) 					;
 source $pwd/../../common/functions.sh					;
 #########################################################################
-command=" sudo docker swarm init | grep token --max-count 1 " 		;
+command=" sudo service docker status | grep running -q && echo OK "	;
 targets=" InstanceManager1 " 						;
 for target in $targets 							;
 do									\
@@ -22,6 +22,16 @@ do									\
     send_list_command "$command" "$target" "$stack"			\
   )"									;
   echo $output								;
+done 									;
+#########################################################################
+command=" sudo docker swarm init | grep token --max-count 1 " 		;
+targets=" InstanceManager1 " 						;
+for target in $targets 							;
+do									\
+  echo "Waiting for $target to complete ..."				;
+  output="$(								\
+    send_list_command "$command" "$target" "$stack"			\
+  )"									;
 done 									;
 token_worker=" $output "						;
 #########################################################################
@@ -37,6 +47,17 @@ do									\
 done 									;
 token_manager=" $output "						;
 #########################################################################
+command=" sudo service docker status | grep running -q && echo OK "	;
+targets=" InstanceManager2 InstanceManager3 " 				;
+for target in $targets 							;
+do									\
+  echo "Waiting for $target to complete ..."				;
+  output="$(								\
+    send_list_command "$command" "$target" "$stack"			\
+  )"									;
+  echo $output								;
+done 									;
+#########################################################################
 command=" sudo $token_manager " 					;
 targets=" InstanceManager2 InstanceManager3 " 				;
 for target in $targets 							;
@@ -47,6 +68,17 @@ do 									\
   )"									;
   echo $output								;
 done									;
+#########################################################################
+command=" sudo service docker status | grep running -q && echo OK "	;
+targets=" InstanceWorker1 InstanceWorker2 InstanceWorker3 " 		;
+for target in $targets 							;
+do									\
+  echo "Waiting for $target to complete ..."				;
+  output="$(								\
+    send_list_command "$command" "$target" "$stack"			\
+  )"									;
+  echo $output								;
+done 									;
 #########################################################################
 command=" sudo $token_worker " 						;
 targets=" InstanceWorker1 InstanceWorker2 InstanceWorker3 " 		;
