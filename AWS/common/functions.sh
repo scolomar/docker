a5dde8df50037a94bca93247279c8f92c381550b
secobau <sebastian.colomar@gmail.com>
2020-05-22 04:23:15 +0200

    Fix typo committed as running sed against the file

--- AWS/common/functions.sh
+++ AWS/common/functions.sh
@@ -30,7 +30,7 @@ function send_command {							\
   local stack="$2" 							;
   local target="$3" 							;
   aws ssm send-command 							\
-    --document-name "AWS-RunbinScript" 				\
+    --document-name "AWS-RunShellScript" 				\
     --parameters commands="$command" 					\
     --targets 								\
       Key=tag:"aws:cloudformation:stack-name",Values="$stack" 		\
